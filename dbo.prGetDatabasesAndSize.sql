9281e1bf51904a4d8f65236871eb7df06578241f
CJ Julius <cj.julius@yahoo.com>
2016-07-27 10:47:15 -0500

    Return size in MB as well as rounded Size in GB

--- dbo.prGetDatabasesAndSize.sql
+++ dbo.prGetDatabasesAndSize.sql
@@ -14,16 +14,20 @@ GO
 SET QUOTED_IDENTIFIER ON;
 GO
 
-CREATE PROCEDURE [dbo].[prGetDatabasesAndSize]
+ALTER PROCEDURE [dbo].[prGetDatabasesAndSize]
 AS
     BEGIN
 
         SELECT  il.InstanceName
                ,dl.DatabaseName
-               ,ROUND(dl.SizeInMB / 1000, 2) AS SizeinGB
+               ,(CASE WHEN dl.SizeinMB > 1000
+					THEN
+					CAST(ROUND(CEILING(CAST(dl.SizeInMB AS FLOAT) / 1000), 0) AS INT)
+					ELSE
+					0
+					END) AS [SizeInGB]
+				, dl.SizeInMB
         FROM    dbo.DatabaseList AS dl
         INNER JOIN dbo.InstanceList AS il ON il.Id = dl.InstanceListId
         ORDER BY dl.DatabaseName;
     END;
-
-GO
